89d097a22495535299e109ed6d7c537253acb17c
zxz100493 <zxz100493@163.com>
2023-07-27 08:38:09 +0800

    init project

---
+++ src/cli.rs
@@ -0,0 +1,46 @@
+// mod cli;
+// use std::path::PathBuf;
+
+// pub enum Action {
+//     Add { task: String },
+//     Done { position: usize },
+//     List,
+// }
+
+// pub struct CommandLineArgs {
+//     pub action: Action,
+//     pub journal_file: Option<PathBuf>,
+// }
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    /// Write tasks to the journal file.
+    Add {
+        /// The task description text.
+        #[structopt()]
+        text: String,
+    },
+    /// Remove an entry from the journal file by position.
+    Done {
+        #[structopt()]
+        position: usize,
+    },
+    /// List all tasks in the journal file.
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Rusty Journal",
+    about = "A command line to-do app written in Rust"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+
+    /// Use a different journal file.
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
