1ea08d38a7dd77797f16d8634b05d39673d756dc
zxz100493 <zxz10049@163.com>
2023-07-30 14:52:16 +0800

    complete demo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 mod cli;
-use structopt::StructOpt;
+use std::path::PathBuf;
 
-fn main() {
-    cli::CommandLineArgs::from_args();
+use anyhow::Ok;
+use structopt::StructOpt;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
+fn main() -> anyhow::Result<()> {
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+    Ok(())
+}
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,12 @@
-use chrono::{serde::ts_second, DateTime, Local, Utc};
+use std::{fmt, fs::File, fs::OpenOptions};
 
-#[derive(Debug)]
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
 
@@ -8,15 +14,15 @@ pub struct Task {
     pub created_at: DateTime<Utc>,
 }
 
-impl Tash {
-    fn new(text: string) -> Tash {
+impl Task {
+    pub fn new(text: String) -> Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
 }
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
@@ -25,7 +31,7 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     let mut tasks = collect_tasks(&file)?;
 
     tasks.push(task);
-    serde_reader::to_writer_pretty(file, &tasks)?;
+    serde_json::to_writer_pretty(file, &tasks)?;
     Ok(())
 }
 
@@ -38,4 +44,43 @@ fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
     };
     file.seek(SeekFrom::Start(0))?; // Rewind the file after.
     Ok(tasks)
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+    let mut tasks = collect_tasks(&file)?;
+
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "invalid task id"));
+    }
+    tasks.remove(task_position - 1);
+    file.set_len(0)?;
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+
+    if tasks.is_empty() {
+        println!("No tasks")
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}:{}", order, task);
+            order += 1;
+        }
+    }
+    Ok(())
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F  %H:%M");
+        write!(f, "{:<50}{}", self.text, created_at)
+    }
 }
